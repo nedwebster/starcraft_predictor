56f17a5c472097febc3d6a029bd037242b1cb30d
epw505 <edwardpwebter@gmail.com>
2022-03-07 17:37:37 +0000

    add initial model files

---
+++ starcraft_predictor/modelling/__init__.py
@@ -0,0 +1,2 @@
+from .model import StarcraftModel
+from .pipeline import sc2_preprocessing_pipeline
---
+++ starcraft_predictor/modelling/model.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import xgboost as xgb
+
+from starcraft_predictor.modelling import model_params
+
+
+class StarcraftModel:
+
+    def __init__(self, xgb_model: xgb.core.Booster):
+        self.model = xgb_model
+
+    @classmethod
+    def train_model(cls, data: pd.DataFrame):
+
+        # HOTFIX: Should be done as a transformer in pipeline
+        for col in model_params.FEATURES:
+            data[col] = data[col].astype("float")
+
+        dmatrix = xgb.DMatrix(
+            data=data[model_params.FEATURES],
+            label=data[model_params.RESPONSE],
+        )
+
+        xgb_model = xgb.train(
+            params=model_params.PARAMS,
+            dtrain=dmatrix,
+            num_boost_round=model_params.NUM_BOOST_ROUND,
+        )
+
+        return cls(xgb_model=xgb_model)
+
+
+    def predict(self, data: pd.DataFrame):
+
+        for col in model_params.FEATURES:
+            data[col] = data[col].astype("float")
+
+        dmatrix = xgb.DMatrix(
+            data=data[model_params.FEATURES],
+        )
+
+        predictions = self.model.predict(dmatrix)
+
+        return predictions
