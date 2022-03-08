93b88c7ddc0d9561c542fa8b739f0a3a7eb5b27c
epw505 <edwardpwebter@gmail.com>
2022-03-08 08:59:27 +0000

    update init file

--- starcraft_predictor/__init__.py
+++ starcraft_predictor/__init__.py
@@ -1,4 +1,6 @@
 # flake8: noqa
-from starcraft_predictor.plot_engine import PlotEngine
-from starcraft_predictor.replay_engine import ReplayEngine
-from starcraft_predictor.replay import Replay
+from .plot_engine import PlotEngine
+from .replay_engine import ReplayEngine
+from .replay import Replay
+from .modelling import StarcraftModel
+from .modelling import sc2_preprocessing_pipeline
