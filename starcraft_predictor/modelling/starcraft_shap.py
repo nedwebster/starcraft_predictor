4210264f4242fbf845fe92057be8a35ad4dc67f4
Ned Webster <ned.webster@simplybusiness.co.uk>
2022-10-05 07:23:25 +0100

    add initial TTD code

---
+++ starcraft_predictor/modelling/starcraft_shap.py
@@ -0,0 +1,24 @@
+class StarcraftShap:
+
+    def __init__(self, processed_replay, features, predictions, model):
+        pass
+
+    def _get_min_max_indexes(self):
+        pass
+
+    def _get_difference(self, index_pair):
+        pass
+
+    def _get_moment_index(self):
+        pass
+
+    def _get_shap_values(self, model, processed_replay, features):
+        pass
+
+    def _get_feature(self, shap_values, index_pair):
+        pass
+
+    def _get_feature_difference(
+        self, shap_values, processed_replay, index_pair
+    ):
+        pass
---
+++ tests/modelling/test_starcraft_shap.py
@@ -0,0 +1,114 @@
+import pytest
+import shap
+from starcraft_predictor.modelling import starcraft_shap
+
+
+class DummyShapExplainer():
+
+    def shap_values(self, x):
+        return "shap_test"
+
+
+class TestStarcraftShap:
+
+    @pytest.mark.xfail
+    def test_init(self, mocker):
+
+        mocker.patch.object(
+            starcraft_shap.StarcraftShap,
+            "_get_moment_index",
+            return_value="test"
+        )
+
+        sc_shap = starcraft_shap.StarcraftShap(
+            processed_replay="test",
+            features=["test_feature"],
+            predictions=[0],
+            model="test",
+        )
+
+        assert sc_shap.predictions == [0]
+        assert sc_shap.features == ["test_feature"]
+        assert sc_shap.moment_index == "test"
+
+    @pytest.mark.xfail
+    @pytest.mark.parametrize(
+        "predictions, expected_output", [
+            ([], []),
+            ([-1, 1, 2], [[0, 2]]),
+            ([0, 3, 4, 2, -1, 3], [0, 2, [2, 4], [4, 5]]),
+        ]
+    )
+    def test_get_min_max_indexes(self, predictions, expected_output):
+
+        sc_shap = starcraft_shap.StarcraftShap(
+            processed_replay="test",
+            features=["test_feature"],
+            predictions=predictions,
+            model="test",
+        )
+
+        output = sc_shap._get_min_max_indexes()
+
+        assert output == expected_output
+
+    @pytest.mark.xfail
+    @pytest.mark.parametrize(
+        "index_pair, expected_output", [
+            ([0, 1], 10),
+            ([1, 3], -8),
+            ([2, 4], 5),
+        ]
+    )
+    def test_get_differece(self, index_pair, expected_output):
+
+        sc_shap = starcraft_shap.StarcraftShap(
+            processed_replay="test",
+            features=["test_feature"],
+            predictions=[0, 10, 3, 2, 15],
+            model="test",
+        )
+
+        output = sc_shap._get_difference(index_pair)
+
+        assert output == expected_output
+
+    @pytest.mark.xfail
+    @pytest.mark.parametrize(
+        "predictions, expected_output", [
+            ([0, 1, 4, 8, 5], [0, 3]),
+            ([4, 6, 3, 1, -1, 2], [1, 4]),
+        ]
+    )
+    def test_get_moment_index(self, predictions, expected_output):
+
+        sc_shap = starcraft_shap.StarcraftShap(
+            processed_replay="test",
+            features=["test_feature"],
+            predictions=predictions,
+            model="test",
+        )
+
+        output = sc_shap._get_moment_index()
+
+        assert output == expected_output
+
+    @pytest.mark.xfail
+    def test_get_shap_values(self, mocker):
+
+        mocker.patch.object(
+            shap,
+            "TreeExplainer",
+            return_value=DummyShapExplainer(),
+        )
+
+        sc_shap = starcraft_shap.StarcraftShap(
+            processed_replay="test",
+            features=["test_feature"],
+            predictions=[0, 1],
+            model="test",
+        )
+
+        output = sc_shap._get_shap_values()
+
+        assert output == "shap_test"
