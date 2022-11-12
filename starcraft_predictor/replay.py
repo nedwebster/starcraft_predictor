1da659a15dbc7ff79e00d21627add1feee6a39b0
Roland <rolandmwebster@gmail.com>
2022-11-12 02:48:52 -0800

    Remove refactored replay method

--- starcraft_predictor/replay.py
+++ starcraft_predictor/replay.py
@@ -81,31 +81,6 @@ class Replay:
 
         return cls.from_replay(replay=replay)
 
-    def get_player_events(self) -> list:
-        """Return all events that are relevant to the players statistics."""
-
-        player_events = [
-            event for event in self.events
-            if event.name == "PlayerStatsEvent"
-        ]
-
-        def events_hotfix(player_events: List[PlayerStatsEvent]):
-            """Remove any non-paired events from the end of the player
-            events list. Non-paired means any timestamped events where
-            only one player has a player event"""
-
-            for i, event in enumerate(player_events):
-
-                if i + 1 == len(player_events):
-                    return player_events
-                else:
-                    if event.player == player_events[i + 1].player:
-                        return player_events[:i]
-
-        fixed_player_events = events_hotfix(player_events)
-
-        return fixed_player_events
-
     def __repr__(self):
         return (
             f"Replay(filehash={self.filehash}, "
